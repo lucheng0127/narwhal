6bbf1fe1477469558d40703ec23446bdd6ba49ff
cheng.lu <lucheng0127@outlook.com>
2022-06-06 22:52:15 +0800

    Add demo
    Only handle one single connection in demo
    
    TODO: Refector connManager

---
+++ demo/client/client.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+var connMap = make(map[int]net.Conn)
+
+func dialServer(port int) int {
+	addr := fmt.Sprintf("127.0.0.1:%d", port)
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	lAddr := conn.LocalAddr()
+	lPort := lAddr.(*net.TCPAddr).Port
+	fmt.Printf("Connect to server with local port %d\n", lPort)
+	connMap[lPort] = conn
+	return lPort
+}
+
+func forwardTraffic(srcConnPort, dstConnPort int) {
+	for {
+		buf := make([]byte, 1024)
+		n, err := connMap[srcConnPort].Read(buf)
+		if err != nil {
+			panic(err)
+		}
+		connMap[dstConnPort].Write(buf[:n])
+	}
+}
+
+func main() {
+	// Dial 7023
+	transferPort := dialServer(7023)
+
+	// Dial 8000
+	forwardPort := dialServer(8000)
+
+	// Forward traffic
+	var wg sync.WaitGroup
+	go forwardTraffic(transferPort, forwardPort)
+	go forwardTraffic(forwardPort, transferPort)
+	wg.Add(2)
+	wg.Wait()
+}
